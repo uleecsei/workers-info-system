2d31cdcf8f3e7e9b5ce26776dc57894c398339fd
Alexei <uleecsei@gmail.com>
2020-06-17 13:28:42 +0300

    lab 10

--- BLL/Services/Impl/WorkerService.ts
+++ BLL/Services/Impl/WorkerService.ts
@@ -3,9 +3,11 @@ import IUnitOfWork from "../../../DAL/UnityOfWork/IUnitOfWork";
 import { SecurityContext } from "../../../CLL/Security/SecurityContext";
 import { User } from "../../../CLL/Security/Identitty/User";
 import { Post } from "../../../DAL/Entities/Post";
+import { Worker } from "../../../DAL/Entities/Worker";
 
 export default class WorkerService implements IWorkerService{
     _user!: User;
+    _worker!: Worker
     constructor(private iUnitOfWork: IUnitOfWork, private securityContext: SecurityContext){
     }
     GetWorkerPost(id: number): Post | null | undefined{
@@ -33,5 +35,8 @@ export default class WorkerService implements IWorkerService{
         catch(error){
            console.error(error); 
         }
+    }
+    get Worker(){
+        return this._worker;
     }
 }
